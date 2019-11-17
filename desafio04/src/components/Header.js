7164066a848b212624ef74e3a25b7b41e5cdf463
Saron Philippi <saron.ph@gmail.com>
2019-11-16 22:26:54 -0300

    Add desafio04 - Dracula Theme

---
+++ desafio04/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="https://i.imgur.com/KDIDiSE.png" />
+        <div>
+          <a href="">Meu perfil</a>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

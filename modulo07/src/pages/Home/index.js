b56d94696e0fa8be7a8bbe8eab2b7306c1f6268c
Saron <saron.ph@gmail.com>
2019-12-04 23:42:18 -0300

    Start flux project, global and home styles

---
+++ modulo07/src/pages/Home/index.js
@@ -0,0 +1,130 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT-Ehk5egsqQlRdJgMOE_1fzNQ8jSjUaKbJabGI_xZ57WMImCzxZzUH2H2l3ftrPlaKaNy1bSh_lQ&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Melhor tênis</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT-Ehk5egsqQlRdJgMOE_1fzNQ8jSjUaKbJabGI_xZ57WMImCzxZzUH2H2l3ftrPlaKaNy1bSh_lQ&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Melhor tênis</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT-Ehk5egsqQlRdJgMOE_1fzNQ8jSjUaKbJabGI_xZ57WMImCzxZzUH2H2l3ftrPlaKaNy1bSh_lQ&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Melhor tênis</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT-Ehk5egsqQlRdJgMOE_1fzNQ8jSjUaKbJabGI_xZ57WMImCzxZzUH2H2l3ftrPlaKaNy1bSh_lQ&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Melhor tênis</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT-Ehk5egsqQlRdJgMOE_1fzNQ8jSjUaKbJabGI_xZ57WMImCzxZzUH2H2l3ftrPlaKaNy1bSh_lQ&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Melhor tênis</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT-Ehk5egsqQlRdJgMOE_1fzNQ8jSjUaKbJabGI_xZ57WMImCzxZzUH2H2l3ftrPlaKaNy1bSh_lQ&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Melhor tênis</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT-Ehk5egsqQlRdJgMOE_1fzNQ8jSjUaKbJabGI_xZ57WMImCzxZzUH2H2l3ftrPlaKaNy1bSh_lQ&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Melhor tênis</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}

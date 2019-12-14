0c85dad8929da27a6b6cac730e67bf9d9f8c0413
Saron <saron.ph@gmail.com>
2019-12-13 22:47:09 -0300

    Code refactoring with react hooks

---
+++ modulo08/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+function App() {
+  const [tech, setTech] = useState([
+    // 'ReactJS', 'React Native'
+  ]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  /*
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+  */
+
+  // busca infos do storage, estilo didMount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // estilo didUpdate
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
+}
+
+export default App;

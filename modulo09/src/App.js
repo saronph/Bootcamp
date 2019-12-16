a11c89a829fef6d7f8390befedfd9f2a3b4b1808
Saron <saron.ph@gmail.com>
2019-12-15 23:44:32 -0300

    Initial setup for frontend integration

---
+++ modulo09/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
+
+import store from './store';
+
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;

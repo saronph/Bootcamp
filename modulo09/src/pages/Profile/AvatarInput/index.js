ed386f817a40e0bcb63b454c96dd5289e2d34494
Saron <saron.ph@gmail.com>
2019-12-20 07:13:19 -0300

    Dashboard finished

---
+++ modulo09/src/pages/Profile/AvatarInput/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { useField } from '@rocketseat/unform';
+import api from '~/services/api';
+
+import { Container } from './styles';
+
+export default function AvatarInput() {
+  const { defaultValue, registerField } = useField('avatar');
+
+  const [file, setFile] = useState(defaultValue && defaultValue.id);
+  const [preview, setPreview] = useState(defaultValue && defaultValue.url);
+
+  const ref = useRef();
+
+  /* Need fix
+  --Maximum update depth exceeded. This can happen when a component
+  calls setState inside useEffect, but useEffect either doesn't have a
+  dependency array, or one of the dependencies changes on every render.--
+
+  useEffect(() => {
+    if (ref.current) {
+      registerField({
+        name: 'avatar_id',
+        ref: ref.current,
+        path: 'dataset.file',
+      });
+    }
+  }, [ref, registerField]);
+  */
+
+  async function handleChange(e) {
+    const data = new FormData();
+
+    data.append('file', e.target.files[0]);
+
+    const response = await api.post('files', data);
+
+    const { id, url } = response.data;
+
+    setFile(id);
+    setPreview(url);
+  }
+
+  return (
+    <Container>
+      <label htmlFor="avatar">
+        <img
+          src={
+            preview || 'https://api.adorable.io/avatars/50/abott@adorable.png'
+          }
+          alt=""
+        />
+        <input
+          type="file"
+          id="avatar"
+          accept="image/*"
+          data-file={file}
+          onChange={handleChange}
+          ref={ref}
+        />
+      </label>
+    </Container>
+  );
+}

0e487168bad4f3190d8c717dec901cbe54d22044
Saron <saron.ph@gmail.com>
2020-03-24 22:28:00 -0300

    Added react-native start project

---
+++ modulo10/src/index.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+
+import Routes from './routes';
+
+// import { Container } from './styles';
+
+export default function Index() {
+  return <Routes />;
+}

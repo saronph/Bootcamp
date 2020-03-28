abc14d66067412779d1a634de326a4ff583b1dc0
Saron <saron.ph@gmail.com>
2020-03-28 16:42:19 -0300

    Added dashboard layout

--- modulo10/src/pages/Dashboard/index.js
+++ modulo10/src/pages/Dashboard/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, {useEffect, useState} from 'react';
+import api from '~/services/api';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
 
-// import { Container } from './styles';
+import {Container, Title, List} from './styles';
 
 export default function Dashboard() {
-  return <Background />;
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({item}) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }

cd60e58a244f6c31c0a7d8502bcd832b9c4f9219
Saron <saron.ph@gmail.com>
2020-04-02 22:54:20 -0300

    Added Appointment page

---
+++ modulo10/src/pages/New/SelectProvider/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function SelectProvider() {
+  return <Background />;
+}
+
+SelectProvider.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.shape,
+  }).isRequired,
+};

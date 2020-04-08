cd41b10285e6ef6e3cb26177c69060c9494c0c99
Saron <saron.ph@gmail.com>
2020-04-07 21:25:15 -0300

    Added Appointment register

--- modulo10/src/pages/New/Confirm/index.js
+++ modulo10/src/pages/New/Confirm/index.js
@@ -1,8 +1,66 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useMemo} from 'react';
+import PropTypes from 'prop-types';
+import {formatRelative, parseISO} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-export default function Confirm() {
-  return <View />;
+import {Container, Avatar, Time, SubmitButton, Name} from './styles';
+
+export default function Confirm({navigation, route}) {
+  const {provider, time} = route.params;
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), {locale: pt}),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      time: PropTypes.string,
+      provider: PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string,
+        avatar: PropTypes.shape({
+          url: PropTypes.string,
+        }),
+      }),
+    }),
+  }).isRequired,
+};
